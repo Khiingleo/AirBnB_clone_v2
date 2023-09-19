e4066716f2ca28a0dc9d158dc5b9c96fb7b80790
Shalom Ogoziem <shalomzymike@gmail.com>
2023-09-19 19:37:42 +0000

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnd_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnd_dev'@'localhost';
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnd_dev'@'localhost';
 FLUSH PRIVILEGES
