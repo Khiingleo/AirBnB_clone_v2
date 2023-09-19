e10dbacde5fe4391df1ed01c624a325037900d88
Shalom Ogoziem <shalomzymike@gmail.com>
2023-09-19 19:36:01 +0000

    task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- script that prepares a MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnd_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnd_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnd_dev'@'localhost';
+FLUSH PRIVILEGES
