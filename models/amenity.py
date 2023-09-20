d0d365b662b3cf51690cb77e057bb76dc06a02fd
Shalom Ogoziem <shalomzymike@gmail.com>
2023-09-20 21:59:08 +0000

    fix error

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, String
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -11,6 +11,6 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = 'amenities'
 
-    name = Column(String(128), nullable=False))
+    name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
                                    viewonly=False)
