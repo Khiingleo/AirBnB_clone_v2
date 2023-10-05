693befd617bd903a93c9ba35fb1a2419c308776a
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-05 07:36:23 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,13 +14,13 @@ fi
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # create fake HTML file to test nginx config
-echo "Test file to check nginx config" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
 # create symbolic link if it doesn't exist, if it does, delete and recreate
 if [ -L /data/web_static/current ]; then
 	sudo rm /data/web_static/current
 fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # give ownership of /data/ folder to ubuntu user AND group(recursive)
 sudo chown -R ubuntu:ubuntu /data/
