fe8ac5bfad94abcaf2e6b55716f94435712537f0
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-20 15:08:57 +0000

    task 8(still fixing)

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    display a HTML page with a list of all state objects
+    in DBStorage
+    """
+    state_objs = []
+    for s in storage.all(State).values():
+        state_objs.append(s)
+    return render_template("7-states_list.html", state_objs=state_objs)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """After each request, remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
