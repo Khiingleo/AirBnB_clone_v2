5c4f65de837a5a096fec92ef9753e29243dd55e1
Shalom Ogoziem <shalomzymike@gmail.com>
2023-10-20 14:28:56 +0000

    fix task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -30,8 +30,8 @@ def c(text):
     return "C {}".format(text)
 
 
-@app.route("/python")
-@app.route("/python/<text>")
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """displays Python followed by <text>"""
     text = text.replace("_", " ")
